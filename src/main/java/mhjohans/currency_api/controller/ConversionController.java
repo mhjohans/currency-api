15b0e948febb086780e89ca352c7547ce7e02dc7
mhjohans <mhjohans@gmail.com>
2024-06-11 23:03:54 +0300

    feat: add exception mapping to HTTP status codes

--- src/main/java/mhjohans/currency_api/controller/ConversionController.java
+++ src/main/java/mhjohans/currency_api/controller/ConversionController.java
@@ -1,10 +1,13 @@
 package mhjohans.currency_api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.server.ResponseStatusException;
 
 import mhjohans.currency_api.service.ConversionService;
 
@@ -16,7 +19,7 @@ public class ConversionController {
     private ConversionService currencyApiService;
 
     /**
-     * Converts the given amount from one currency to another.
+     * HTTP GET endpoint that converts the given decimal amount from one currency to another.
      *
      * @param  from    the currency code to convert from as a string
      * @param  to      the currency code to convert to as a string
@@ -27,8 +30,15 @@ public class ConversionController {
     public String convertCurrency(@RequestParam String from, @RequestParam String to, @RequestParam double amount) {
         // TODO: Add authentication
         // TODO: Add CSRF and CSP for security
-        // TODO: Add mapping of errors to HTTP status codes
-        return currencyApiService.convertCurrency(from, to, amount);
+        try {
+            return currencyApiService.convertCurrency(from, to, amount);
+        } catch (IllegalArgumentException e) {
+            // Received an invalid request
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage(), e);
+        } catch (RestClientException e) {
+            // Could not get a response from the currency rate API
+            throw new ResponseStatusException(HttpStatus.GATEWAY_TIMEOUT, e.getMessage(), e);
+        }
     }
 
 }
